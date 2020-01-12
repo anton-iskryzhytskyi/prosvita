fbe91238b2bb6529540a9dcf03ba628ec8fae7a1
iskryzhytskyi <a.iskryzhytskyi@gmail.com>
2020-01-12 18:45:40 +0200

    feat(docker-compose-example): add front app

---
+++ 2020-docker/docker-compose-example/front/src/views/App/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../../logo.svg'
+import './index.css'
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Frontend Application
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
