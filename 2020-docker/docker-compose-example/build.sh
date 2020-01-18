20ba4a068b678790ac0b957b75f848d406bde359
iskryzhytskyi <a.iskryzhytskyi@gmail.com>
2020-01-18 13:29:03 +0200

    feat: add runs.sh, fix others

--- 2020-docker/docker-compose-example/build.sh
+++ 2020-docker/docker-compose-example/build.sh
@@ -4,8 +4,8 @@ set -ex
 
 rootPath=$(git rev-parse --show-toplevel)
 
-path1=$rootPath/docker2020/docker-compose-example/front
-docker build -t prosvita-front $path1
+path1="$rootPath"/2020-docker/docker-compose-example/front
+docker build -t prosvita-front "$path1"
 
-path2=$rootPath/docker2020/docker-compose-example/back
-docker build -t prosvita-back $path2
+path2="$rootPath"/2020-docker/docker-compose-example/back
+docker build -t prosvita-back "$path2"
